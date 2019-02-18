ca77417f583a79e5863181b04ef42b09f4c5c098
Fp38SilSha <faperez38s@gmail.com>
2019-02-18 16:06:30 -0600

    All classes needed, created. Add user story method names.

---
+++ SweepStakes/SweepStakes/Sweepstakes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Sweepstakes
+    {
+        public Sweepstakes(string name)
+        {
+
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+
+
+        }
+
+        public string PickWinner()
+        {
+
+
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
